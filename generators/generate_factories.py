fd672b2ccb7422b9a016aa78e780c3238b173199
Laura Jimenez <laufjimdev@gmail.com>
2026-07-10 11:36:15 -0700

    feat - Added db connection logic, created factories data generator.

---
+++ generators/generate_factories.py
@@ -0,0 +1,63 @@
+from db_connection import get_connection
+
+factories = [
+
+    ("F1",
+     "Dallas–Fort Worth Factory",
+     "Dallas–Fort Worth",
+     "Texas",
+     1200),
+
+    ("F2",
+     "Atlanta Metro Factory",
+     "Atlanta",
+     "Georgia",
+     1000),
+
+    ("F3",
+     "Phoenix–Buckeye Factory",
+     "Buckeye",
+     "Arizona",
+     900)
+
+]
+
+connection = get_connection()
+
+cursor = connection.cursor()
+
+insert_query = """
+
+INSERT INTO factories
+(
+    factory_id,
+    factory_name,
+    city,
+    state,
+    capacity_units_per_day,
+    manager_employee_id
+)
+
+VALUES
+(
+    %s,
+    %s,
+    %s,
+    %s,
+    %s,
+    NULL
+);
+
+"""
+
+for factory in factories:
+
+    cursor.execute(insert_query, factory)
+
+connection.commit()
+
+cursor.close()
+
+connection.close()
+
+print("Factories inserted successfully.")
